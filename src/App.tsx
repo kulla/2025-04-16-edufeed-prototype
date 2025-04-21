a5dff4719b8663b3d62d0c1f5611a9b108b792af
Stephan Kulla <git.mail@kulla.me>
2025-04-21 15:24:16 +0200

    feat: Add AppShell

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
 import '@mantine/core/styles.css'
-import { Button, createTheme, MantineProvider } from '@mantine/core'
+import { createTheme, MantineProvider, AppShell, Burger } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
 
 const theme = createTheme({})
 
 const App = () => {
   return (
     <MantineProvider theme={theme}>
-      <Button variant="filled" color="blue">
-        Button
-      </Button>
+      <Demo />
     </MantineProvider>
+  )
+}
+
+function Demo() {
+  const [opened, { toggle }] = useDisclosure()
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header p="sm">
+        <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
   )
 }
 
