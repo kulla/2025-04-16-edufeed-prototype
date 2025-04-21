e752e3a0661dc814345278e4660aabc34c7e15b2
Stephan Kulla <git.mail@kulla.me>
2025-04-21 21:38:57 +0200

    feat: Create user

--- src/components/add-account.tsx
+++ src/components/add-account.tsx
@@ -1,8 +1,49 @@
+import { TextInput, Group, Button, Title, Container } from '@mantine/core'
+import { useForm } from '@mantine/form'
+import useEventLog from '../hooks/event-log'
+
 export default function AddAccount() {
+  const events = useEventLog()
+  const form = useForm({
+    initialValues: {
+      userName: '',
+    },
+
+    validate: {
+      userName(value) {
+        if (value === '') {
+          return 'Es muss ein Benutzername angegeben werden'
+        }
+        if (value.length > 20) {
+          return 'Benutzername darf maximal 20 Zeichen lang sein'
+        }
+        if (value.length < 3) {
+          return 'Benutzername muss mindestens 3 Zeichen lang sein'
+        }
+        return null
+      },
+    },
+  })
+
+  const handleSubmit = (name: string) => {
+    events.addEvent({ type: 'create-user', name })
+  }
+
   return (
-    <div>
-      <h1>Add Account</h1>
-      <p>Here you can add a new account.</p>
-    </div>
+    <Container size="xs" ml="0">
+      <Title order={2} mb="md">
+        Neuen Account hinzufügen
+      </Title>
+      <form onSubmit={form.onSubmit(({ userName }) => handleSubmit(userName))}>
+        <TextInput
+          label="Benutzername"
+          placeholder="Benutzername eingeben"
+          {...form.getInputProps('userName')}
+        />
+        <Group justify="flex-end" mt="md">
+          <Button type="submit">Benutzername hinzufügen</Button>
+        </Group>
+      </form>
+    </Container>
   )
 }
---
+++ src/hooks/event-log.tsx
@@ -0,0 +1,40 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  useCallback,
+  type ReactNode,
+} from 'react'
+
+type Event = CreateUserEvent
+
+interface CreateUserEvent {
+  type: 'create-user'
+  name: string
+}
+
+const EventLogContext = createContext<{
+  events: Event[]
+  addEvent: (event: Omit<Event, 'eventId'>) => void
+}>({
+  events: [],
+  addEvent: () => {},
+})
+
+export function EventLogProvider({ children }: { children: ReactNode }) {
+  const [events, setEvents] = useState<Event[]>([])
+
+  const addEvent = useCallback((event: Event) => {
+    setEvents((prevEvents) => [...prevEvents, event])
+  }, [])
+
+  return (
+    <EventLogContext.Provider value={{ events, addEvent }}>
+      {children}
+    </EventLogContext.Provider>
+  )
+}
+
+export default function useEventLog() {
+  return useContext(EventLogContext)
+}
