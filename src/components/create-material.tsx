ee870060a88a299b9e2b00fcdb564af9ffc8bbcd
Stephan Kulla <git.mail@kulla.me>
2025-04-22 08:10:14 +0200

    wip: Create educational material view

--- src/components/create-material.tsx
+++ src/components/create-material.tsx
@@ -1,3 +1,29 @@
+import { Button, Group, Title, Modal, Textarea } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+
 export default function CreateMaterial({ account }: { account: string }) {
-  return <h1>Create Material {account}</h1>
+  const [opened, { open, close }] = useDisclosure(false)
+
+  return (
+    <>
+      <Title order={2} mb="lg">
+        Meine Lernmaterialien (von „{account}“)
+      </Title>
+      <Group mb="lg">
+        <Button onClick={open}>Neues Lernmaterial erstellen</Button>
+      </Group>
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="Neues Lernmaterial erstellen"
+      >
+        <Textarea
+          label="Lernmaterial"
+          placeholder="Geben Sie hier Ihr Lernmaterial ein."
+          autosize
+          minRows={5}
+        />
+      </Modal>
+    </>
+  )
 }
