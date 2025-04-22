ebafb5e6b7c2d494aa4d81eeddf699635f7661cf
Stephan Kulla <git.mail@kulla.me>
2025-04-22 19:30:41 +0200

    feat(explore): Show that Learning material was liked

--- src/components/explore.tsx
+++ src/components/explore.tsx
@@ -10,24 +10,12 @@ export default function Explore({ account }: { account: string }) {
       .filter((event) => event.type === 'create-learning-material')
       .filter((event) => event.account !== account),
   )
-
-  const handleLike = (accountName: string, content: string) => {
-    addEvent({
-      type: 'like',
-      actor: account,
-      account: accountName,
-      content,
-    })
-  }
-
-  const handleCurateOER = (accountName: string, content: string) => {
-    addEvent({
-      type: 'curate-oer',
-      actor: account,
-      account: accountName,
-      content,
-    })
-  }
+  const likedContents = events
+    .filter((event) => event.type === 'like')
+    .filter((event) => event.actor === account)
+  const curatedOERs = events
+    .filter((event) => event.type === 'curate-oer')
+    .filter((event) => event.actor === account)
 
   return (
     <Container size="sm" ml={0}>
@@ -35,31 +23,70 @@ export default function Explore({ account }: { account: string }) {
         Inhalter anderer Accounts
       </Title>
       {otherContents.map((event) => (
-        <Card key={event.eventId} shadow="sm" padding="lg" mt="md" withBorder>
-          <Title order={3} mb="md">
-            OER von „{event.account}”:
-          </Title>
-          <Text style={{ whiteSpace: 'pre-wrap' }}>{event.content}</Text>
-          <Group gap={10} mt="md">
-            <Button
-              leftSection={<Heart size={16} />}
-              variant="light"
-              color="red"
-              onClick={() => handleLike(event.account, event.content)}
-            >
-              Like
-            </Button>
-            <Button
-              leftSection={<BookmarkCheck size={16} />}
-              variant="light"
-              color="green"
-              onClick={() => handleCurateOER(event.account, event.content)}
-            >
-              Wertvolles OER
-            </Button>
-          </Group>
-        </Card>
+        <OERCard key={event.eventId} event={event} />
       ))}
     </Container>
   )
+
+  function OERCard({ event }: { event: (typeof otherContents)[number] }) {
+    const isLiked = likedContents.some(
+      (likedEvent) =>
+        likedEvent.content === event.content &&
+        likedEvent.account === event.account,
+    )
+    const isCurated = curatedOERs.some(
+      (curatedEvent) =>
+        curatedEvent.content === event.content &&
+        curatedEvent.account === event.account,
+    )
+
+    const handleLike = () => {
+      if (!isLiked) {
+        addEvent({
+          type: 'like',
+          actor: account,
+          account: event.account,
+          content: event.content,
+        })
+      }
+    }
+
+    const handleCurateOER = () => {
+      if (!isCurated) {
+        addEvent({
+          type: 'curate-oer',
+          actor: account,
+          account: event.account,
+          content: event.content,
+        })
+      }
+    }
+
+    return (
+      <Card key={event.eventId} shadow="sm" padding="lg" mt="md" withBorder>
+        <Title order={3} mb="md">
+          OER von „{event.account}”:
+        </Title>
+        <Text style={{ whiteSpace: 'pre-wrap' }}>{event.content}</Text>
+        <Group gap={10} mt="md">
+          <Button
+            leftSection={<Heart size={16} />}
+            variant={isLiked ? 'filled' : 'light'}
+            color="red"
+            onClick={() => handleLike()}
+          >
+            Like
+          </Button>
+          <Button
+            leftSection={<BookmarkCheck size={16} />}
+            variant={isCurated ? 'filled' : 'light'}
+            color="green"
+            onClick={() => handleCurateOER()}
+          >
+            Wertvolles OER
+          </Button>
+        </Group>
+      </Card>
+    )
+  }
 }
