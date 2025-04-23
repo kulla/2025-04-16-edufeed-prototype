0425f9e9ba92f7f9d110566ddd1e029242dbd5ed
Stephan Kulla <git.mail@kulla.me>
2025-04-23 17:23:35 +0200

    chore: Set HTML title

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@rsbuild/core'
 import { pluginReact } from '@rsbuild/plugin-react'
 
 export default defineConfig({
+  html: { title: 'EduFeed Prototype' },
   output: {
     assetPrefix: '/2025-04-16-edufeed-prototype/',
   },
